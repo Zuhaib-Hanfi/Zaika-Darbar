17214f4d6f9f9254a0ccd13705b73bd6b37d604d
Zuhaib Hanfi <hanfizuhaib@gamil.com>
2025-06-01 13:04:52 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-// Login Form
-let logButton = document.getElementById('loginButton');
-let loginDiv = document.getElementById('login');
-let formClose = document.getElementById('formClose');
-let loginForm = document.getElementById('loginForm');
-// Show login form
-logButton.addEventListener('click', (e) => {
-    loginDiv.style.zIndex = 10;
-    e.stopPropagation(); // prevent it from triggering body click
-});
+// // Login Form
+// let logButton = document.getElementById('loginButton');
+// let loginDiv = document.getElementById('login');
+// let formClose = document.getElementById('formClose');
+// let loginForm = document.getElementById('loginForm');
+// // Show login form
+// logButton.addEventListener('click', (e) => {
+//     loginDiv.style.zIndex = 10;
+//     e.stopPropagation(); // prevent it from triggering body click
+// });
 
-// Close when clicking outside
-loginDiv.addEventListener('click', () => {
-    loginDiv.style.zIndex = -10;
-});
+// // Close when clicking outside
+// loginDiv.addEventListener('click', () => {
+//     loginDiv.style.zIndex = -10;
+// });
 
-// Prevent body click when clicking inside the login form
-loginForm.addEventListener('click', (e) => {
-    e.stopPropagation();
-});
+// // Prevent body click when clicking inside the login form
+// loginForm.addEventListener('click', (e) => {
+//     e.stopPropagation();
+// });
 
-// Close with close button
-formClose.addEventListener('click', (e) => {
-    loginDiv.style.zIndex = -10;
-    e.stopPropagation();
-});
+// // Close with close button
+// formClose.addEventListener('click', (e) => {
+//     loginDiv.style.zIndex = -10;
+//     e.stopPropagation();
+// });
 
-// Sign-up Form
-let signButton = document.getElementById('signupButton');
-let signUpDiv = document.getElementById('signUp');
-let signUpForm = document.getElementById('signUpForm');
-let formClose2 = document.getElementById('formClose2');
+// // Sign-up Form
+// let signButton = document.getElementById('signupButton');
+// let signUpDiv = document.getElementById('signUp');
+// let signUpForm = document.getElementById('signUpForm');
+// let formClose2 = document.getElementById('formClose2');
 
-signButton.addEventListener('click', (event) => {
-    signUpDiv.style.zIndex = 11;
-    event.stopPropagation();
-});
-signUpDiv.addEventListener('click', () => {
-    signUpDiv.style.zIndex = -11;
-});
-signUpForm.addEventListener('click', (event) => {
-    event.stopPropagation();
-});
-formClose2.addEventListener('click', (event) => {
-    signUpDiv.style.zIndex = -11;
-    event.stopPropagation();
-});
+// signButton.addEventListener('click', (event) => {
+//     signUpDiv.style.zIndex = 11;
+//     event.stopPropagation();
+// });
+// signUpDiv.addEventListener('click', () => {
+//     signUpDiv.style.zIndex = -11;
+// });
+// signUpForm.addEventListener('click', (event) => {
+//     event.stopPropagation();
+// });
+// formClose2.addEventListener('click', (event) => {
+//     signUpDiv.style.zIndex = -11;
+//     event.stopPropagation();
+// });
 
 
 // Hamburger Menu Toggle
